16735c627e30a0e7f166a0555ed4185176d1a517
hasifasi <52176150+hasifasi@users.noreply.github.com>
2019-06-24 14:51:51 -0400

    Add files via upload

---
+++ ExcerciseFile.java
@@ -0,0 +1,51 @@
+package ExcercieseInterviewWW;
+
+import java.io.BufferedReader;
+import java.io.File;
+
+import java.io.FileReader;
+
+public class ExcerciseFile {
+	
+	public static void main(String[] args) {
+
+		String path =System.getProperty("user.dir")+ "/" + "WWfile.txt"; 
+		ExcerciseFile.doesFileExist(path);
+
+	}
+
+	public static void doesFileExist(String path) {
+
+		try {
+			File file = new File(path);
+			if (file.exists()) {
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				String value;
+
+				while ((value = br.readLine()) != null) {
+					String[] Word = value.split("-");
+					For1:
+					for (int j=0;j<Word.length;j++){
+						System.out.println(Word[j]);
+						String [] Meaning = Word[j+1].split(",");
+						for (int i = 0; i < Meaning.length; i++) {
+							System.out.println(Meaning[i]);
+						}
+					break For1;
+					}	
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Exception occured while accessing file: " + e);
+			// e.printStackTrace();
+			
+
+		}
+
+	}
+
+	
+	
+	
+
+}
